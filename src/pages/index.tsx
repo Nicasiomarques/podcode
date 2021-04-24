2617ab88262ed29afa94ebd5523d893d8b2c979c
nicasiomarques <nicasiomarques18@gmail.com>
2021-04-24 14:27:28 +0100

    chore: implementing ssg to take episodes from server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+import { calculateRevalidateInHours, MakeAPIUrl } from "../utils"
+
+export default function Home({ episodes }) {
   return (
-    <div>Home</div>
+    <div>{JSON.stringify(episodes)}</div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch(MakeAPIUrl('episodes'))
+  const episodes = await response.json()
+
+  return {
+    props: {
+      episodes
+    },
+    revalidate: calculateRevalidateInHours(8),
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,7 @@ import ptBR from "date-fns/locale/pt-BR";
 export const currentDate = format(new Date(), "EEEEEE, d MMMM", {
   locale: ptBR,
 });
+
+export const MakeAPIUrl = (resource: string) => `http://localhost:8000/${resource}`;
+
+export const calculateRevalidateInHours = (hours: number) => 60 * 60 * hours;
