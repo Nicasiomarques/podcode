319007a55a983ecd9175c28e47a4225b8e21df1d
nicasiomarques <nicasiomarques18@gmail.com>
2021-04-25 08:43:51 +0100

    create home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,125 @@
-import { calculateRevalidateInHours, MakeAPIUrl } from "../utils"
+import { GetStaticProps } from "next"
+import Image from "next/image"
 
-export default function Home({ episodes }) {
+
+import styles from './styles.module.scss'
+
+import { getEpisodes } from "../services/Api"
+import { calculateRevalidateInHours, convertDurationsToTimeString, formatPublishedAt } from "../utils"
+
+interface Episode {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+
+interface HomeProps {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>{JSON.stringify(episodes)}</div>
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Ultimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map((episode => (
+            <li key={episode.id}>
+              <Image
+                width={192}
+                height={192}
+                src={episode.thumbnail}
+                alt={episode.title}
+                objectFit='cover'
+              />
+
+              <div className={styles.episode__details}>
+                <a href={episode.url}>{episode.title}</a>
+                <p>{episode.members}</p>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+              </div>
+
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar episodio" />
+              </button>
+            </li>
+          )))}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>Todos os episodios</h2>
+
+        <table className={styles.allEpisodes} cellSpacing={0}>
+          <thead>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => (
+              <tr key={episode.id}>
+                <td style={{ width: 72 }}>
+                  <Image
+                    width={120}
+                    height={120}
+                    src={episode.thumbnail}
+                    alt={episode.title}
+                    objectFit='cover'
+                  />
+                </td>
+                <td>{episode.title}</td>
+                <td>{episode.members}</td>
+                <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                <td>{episode.durationAsString}</td>
+                <td>
+                  <button>
+                    <img src="/play-green.svg" alt="Tocar episodio" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </div>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch(MakeAPIUrl('episodes'))
-  const episodes = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await getEpisodes()
+
+  const episodes = data.map((episode) => ({
+    id: episode.id,
+    title: episode.title,
+    thumbnail: episode.thumbnail,
+    members: episode.members,
+    description: episode.description,
+    url: episode.file.url,
+    duration: Number(episode.duration),
+    publishedAt: formatPublishedAt(episode.published_at),
+    durationAsString: convertDurationsToTimeString(episode.file.duration)
+  }))
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length)
 
   return {
     props: {
-      episodes
+      latestEpisodes,
+      allEpisodes
     },
     revalidate: calculateRevalidateInHours(8),
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,24 @@
+import { parseISO } from "date-fns";
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
+
+export const formatPublishedAt = (date: string) =>
+  format(parseISO(date), "d MMM yy", { locale: ptBR });
 
 export const currentDate = format(new Date(), "EEEEEE, d MMMM", {
   locale: ptBR,
 });
 
-export const MakeAPIUrl = (resource: string) => `http://localhost:8000/${resource}`;
-
 export const calculateRevalidateInHours = (hours: number) => 60 * 60 * hours;
+
+export const convertDurationsToTimeString = (time: string) => {
+  const duration = Number(time);
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = duration % 60;
+
+  const formattedTime = [hours, minutes, seconds]
+    .map(unit => String(unit).padStart(2, "0"))
+    .join(":");
+  return formattedTime;
+};
