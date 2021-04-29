f3aa8b67683ddd362cd78a8ca94c2b9d44c15964
nicasiomarques <nicasiomarques18@gmail.com>
2021-04-29 14:45:25 +0100

    add slider and customize hover player buttons

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,8 @@
 import { useContext } from 'react'
 import Image from 'next/image'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+
 import { PlayerContext } from '../../contexts/Player'
 import styles from './styles.module.scss'
 
@@ -34,28 +37,39 @@ export default function Player() {
       <footer>
         <div className={styles.player__progress}>
           <span>00:00</span>
-          <div className={styles['player__slider--empty']} />
+          {episode ?
+            (
+              <Slider
+                trackStyle={{ backgroundColor: '#84d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#84d361', borderWidth: 4 }}
+              />
+            ) :
+            (
+              <div className={styles['player__slider--empty']} />
+            )
+          }
           <span>00:00</span>
         </div>
 
         <div className={styles.player__buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Tocar aleatoria" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play.svg" className={styles.play__button} alt="Tocar" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar proxima" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
