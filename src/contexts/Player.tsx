694d31865fee1fde255a8c9bb74dad10804e39c9
nicasiomarques <nicasiomarques18@gmail.com>
2021-04-30 08:00:50 +0100

    move player provider to own file

---
+++ src/contexts/Player.tsx
@@ -0,0 +1,55 @@
+import { createContext, FC, useState } from "react";
+
+interface Episode {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+}
+
+interface PlayerContextData {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  play: (episode: Episode) => void;
+  togglePlay: () => void;
+  setPlayingState: (state: boolean) => void;
+}
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+export const PlayerContextProvider: FC = ({ children }) => {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        currentEpisodeIndex,
+        setPlayingState,
+        episodeList,
+        togglePlay,
+        isPlaying,
+        play,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  )
+}
