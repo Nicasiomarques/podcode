2720f7b1b9f86ee22bda4475adad53e50c25405e
nicasiomarques <nicasiomarques18@gmail.com>
2021-04-30 11:59:56 +0100

    refector: add try catch to api call

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -5,48 +5,58 @@ import { GetEpisodeResponse, GetEpisodesResponse } from "./Api.types";
 export const api = axios.create({ baseURL: "http://localhost:8000" });
 
 export const getEpisodes = async () => {
-  const { data } = await api.get<GetEpisodesResponse[]>("episodes", {
-    params: {
-      _limit: 12,
-      _sort: "published_at",
-      _order: "desc",
-    },
-  });
+  try {
+    const { data } = await api.get<GetEpisodesResponse[]>("episodes", {
+      params: {
+        _limit: 12,
+        _sort: "published_at",
+        _order: "desc",
+      },
+    });
 
-  const episodes = data.map(episode => ({
-    id: episode.id,
-    title: episode.title,
-    thumbnail: episode.thumbnail,
-    members: episode.members,
-    url: episode.file.url,
-    duration: Number(episode.file.duration),
-    publishedAt: formatPublishedAt(episode.published_at),
-    durationAsString: formatDuration(episode.file.duration),
-  }));
+    const episodes = data.map(episode => ({
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      url: episode.file.url,
+      duration: Number(episode.file.duration),
+      publishedAt: formatPublishedAt(episode.published_at),
+      durationAsString: formatDuration(episode.file.duration),
+    }));
 
-  const latestEpisodes = episodes.slice(0, 2);
-  const allEpisodes = episodes.slice(2, episodes.length);
+    const latestEpisodes = episodes.slice(0, 2);
+    const allEpisodes = episodes.slice(2, episodes.length);
 
-  return {
-    latestEpisodes,
-    allEpisodes,
-  };
+    return {
+      latestEpisodes,
+      allEpisodes,
+    };
+  } catch (error) {
+    // Todo: Improve error handling
+    console.error(error);
+  }
 };
 
 export const getEpisode = async (slug: string) => {
-  const { data } = await api.get<GetEpisodeResponse>(`episodes/${slug}`);
+  try {
+    const { data } = await api.get<GetEpisodeResponse>(`episodes/${slug}`);
 
-  const episode = {
-    id: data.id,
-    title: data.title,
-    thumbnail: data.thumbnail,
-    members: data.members,
-    description: data.description,
-    url: data.file.url,
-    duration: Number(data.file.duration),
-    publishedAt: formatPublishedAt(data.published_at),
-    durationAsString: formatDuration(data.file.duration),
-  };
+    const episode = {
+      id: data.id,
+      title: data.title,
+      thumbnail: data.thumbnail,
+      members: data.members,
+      description: data.description,
+      url: data.file.url,
+      duration: Number(data.file.duration),
+      publishedAt: formatPublishedAt(data.published_at),
+      durationAsString: formatDuration(data.file.duration),
+    };
 
-  return episode;
+    return episode;
+  } catch (error) {
+    // Todo: Improve error handling
+    console.error(error);
+  }
 };
