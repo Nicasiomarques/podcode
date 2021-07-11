95286b1f84265ab2abfb0d745a8a11dc3d756c57
Nicasiomarques <nicasiomarques18@gmail.com>
2021-07-11 01:01:24 +0100

    Add types on episodes page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -8,6 +8,21 @@ import { getEpisode } from '../../services/Api'
 import styles from './style.module.scss';
 import { usePlayer } from '../../contexts/Player'
 
+export interface Episode {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+export interface EpisodeProps {
+  episode: Episode;
+}
+
 export default function Episode({ episode }: EpisodeProps) {
   const { play } = usePlayer();
 
